a89a65580bc6019f660dd5baf3148cf6ad501a42
Quimey5 <162723087+Quimey5@users.noreply.github.com>
2024-05-23 20:08:48 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,17 +1,8 @@
 def enumerate_list(list):
-    result = []
-    for e in list:
-        if not e: continue
-        
-        result += [f'{len(result)}. {e}']
     
     return result
 
 
 def enumerate_backwards(list):
-    result = []
-    for e in list:
-        if e != '': 
-            result += [f'{len(result)}. {e[::-1]}']
     
     return result

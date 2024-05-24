5dd2e4f56040a0e048c28a82d9c078138a7016d5
Quimey5 <162723087+Quimey5@users.noreply.github.com>
2024-05-23 22:07:57 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,41 +1,48 @@
-def enumerate_list(colors):
-  
-    new_list = []
+def enumerate_list(strings):
+
+    enumerated_list = []
     
     result_index = 0
-    
-    
-    for color in colors:
-      
-        if color:
-            
-            new_list.append(f"{result_index}. {color}")
-          
+   
+    for string in strings:
+        
+        if string:
+            enumerated_list.append(f"{result_index}. {string}")
+   
             result_index += 1
     
-    return new_list
+    return enumerated_list
+
+colors = ["Red", "Green", "", "White", "Black"]
 
 result = enumerate_list(colors)
 
 print(result)
-def enumerate_list(colors):
-  
 
-    new_list = []
-    
+def enumerate_backwards(strings):
+
+    enumerated_list = []
     
     result_index = 0
     
-    for color in colors:
-
-        if color:
-        
-            new_list.append(f"{result_index}. {color}")
-
+ 
+    for string in strings:
+     
+        if string:
+           
+            reversed_string = string[::-1]
+            enumerated_list.append(f"{result_index}. {reversed_string}")
+           
             result_index += 1
     
-    return new_list
+    return enumerated_list
+
+
+colors = ["Red", "Green", "", "White", "Black"]
+
+
 result = enumerate_backwards(colors)
 
 
 print(result)
+
